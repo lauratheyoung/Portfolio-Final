71e0ca0d3ad5693db449948ae24753254d0c3002
LAURA YOUNG <laurayoung305@gmail.com>
2023-05-29 16:14:55 +1000

    this is a commit

--- public/script.js
+++ public/script.js
@@ -3,14 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const addWorkoutButton = document.querySelector('#addWorkout');
   const form = document.querySelector('#form');
+  const formGroup = document.getElementById('userWorkoutForm');
+  const popup = document.getElementById('congrats');
   const overlay = document.getElementById('overlay');
   const workoutHistoryButton = document.querySelector('#workoutHistory');
   const workoutCardContainer = document.querySelector("#entryCard");
-  const workout = [];
+  let workout = [];
+
+
+  // Load workout data from local storage
+  const storedWorkoutData = localStorage.getItem('workoutData');
+  if (storedWorkoutData) {
+    const parsedWorkoutData = JSON.parse(storedWorkoutData);
+    if (Array.isArray(parsedWorkoutData)) {
+      workout.push(...parsedWorkoutData);
+    } else {
+      workout.push(parsedWorkoutData);
+    }
+  }
 
   // taking in workout entry data and creating an entry object to store using localStorage & using JSON to condense data into string
 
-  const addWorkoutEntry = (workoutDate, startTime, endTime, easy, moderate, difficult, style) => {
+  const addWorkoutEntry = ( workoutDate, startTime, endTime, easy, moderate, difficult, style) => {
+  
+
     const entry = {
       workoutDate,
       startTime,
@@ -20,61 +36,11 @@ document.addEventListener('DOMContentLoaded', () => {
       difficult,
       style
     };
+
     workout.push(entry);
     localStorage.setItem('workoutData', JSON.stringify(workout));
   };
-
   
-
-  const createWorkoutElement = ({ workoutDate, startTime, endTime, easy, moderate, difficult, style }) => {
-    // Create card container
-    const cardContainer = document.createElement('div');
-    cardContainer.classList.add('entryCard');
-
-    // Set background color
-    cardContainer.style.backgroundColor = '#E0C1FF';
-
-    // create text container
-    const cardContainerText = document.createElement('div');
-
-    // Create text elements
-    const workoutDateElement = document.createElement('h1');
-    const workoutStartTimeElement = document.createElement('p');
-    const workoutEndTimeElement = document.createElement('p');
-    const difficultyLevelElement = document.createElement('p');
-    const workoutJumpTypeElement = document.createElement('p');
-
-    // Set text content
-    workoutDateElement.innerText = "Workout Date: " + workoutDate;
-    workoutStartTimeElement.innerText = "Start-time: " + startTime;
-    workoutEndTimeElement.innerText = "End-time: " + endTime;
-    difficultyLevelElement.innerText = "Workout Difficulty: " + getDifficultyLevel(easy, moderate, difficult);
-    workoutJumpTypeElement.innerText = "Jump-Type: " + style;
-
-    // Set text color
-    workoutDateElement.style.color = 'black';
-    workoutStartTimeElement.style.color = 'black';
-    workoutEndTimeElement.style.color = 'black';
-    difficultyLevelElement.style.color = 'black';
-    workoutJumpTypeElement.style.color = 'black';
-
-    cardContainerText.append(workoutStartTimeElement, workoutEndTimeElement, difficultyLevelElement, workoutJumpTypeElement);
-
-    cardContainerText.style.backgroundColor = 'white';
-    cardContainerText.style.padding = "10px";
-
-    // Append text elements to card container
-    cardContainer.append(workoutDateElement, cardContainerText);
-
-    // Append card container to workout card container
-    workoutCardContainer.appendChild(cardContainer);
-
-    // styling applied to dynamically generated cards
-    workoutCardContainer.style.display = "flex";
-    workoutCardContainer.style.flexWrap = "wrap";
-    workoutCardContainer.style.justifyContent = "space-between";
-  };
-
   // this checks and returns the associated workout difficulty-level of each workout entry
   const getDifficultyLevel = (easy, moderate, difficult) => {
     if (easy) {
@@ -88,6 +54,50 @@ document.addEventListener('DOMContentLoaded', () => {
     return 'Unknown';
   };
 
+  
+
+  const createWorkoutElement = ({ workoutDate, startTime, endTime, easy, moderate, difficult, style }) => {
+    // Create card container
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('entryCard');
+  
+    // Set background color
+    cardContainer.style.backgroundColor = '#E0C1FF';
+  
+    // Create text elements
+    const workoutDateElement = document.createElement('h1');
+    const workoutStartTimeElement = document.createElement('p');
+    const workoutEndTimeElement = document.createElement('p');
+    const difficultyLevelElement = document.createElement('p');
+    const workoutJumpTypeElement = document.createElement('p');
+  
+    // Set text content
+    workoutDateElement.innerText = "Workout Date: " + workoutDate;
+    workoutStartTimeElement.innerText = "Start-time: " + startTime;
+    workoutEndTimeElement.innerText = "End-time: " + endTime;
+    difficultyLevelElement.innerText = "Workout Difficulty: " + getDifficultyLevel(easy, moderate, difficult);
+    workoutJumpTypeElement.innerText = "Jump-Type: " + style;
+  
+    // Set text color
+    workoutDateElement.style.color = 'black';
+    workoutStartTimeElement.style.color = 'black';
+    workoutEndTimeElement.style.color = 'black';
+    difficultyLevelElement.style.color = 'black';
+    workoutJumpTypeElement.style.color = 'black';
+  
+    // Append text elements to card container
+    cardContainer.append(workoutDateElement, workoutStartTimeElement, workoutEndTimeElement, difficultyLevelElement, workoutJumpTypeElement);
+  
+    // Append card container to workout card container
+    workoutCardContainer.appendChild(cardContainer);
+    workoutCardContainer.style.display = "none";
+  
+    // Apply styling to dynamically generated cards
+    
+    workoutCardContainer.style.flexWrap = "wrap";
+    workoutCardContainer.style.justifyContent = "space-between";
+  };
+  
 
   // open workout history functionality
   let formCloseButton = null;
@@ -109,20 +119,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const openWorkoutHistory = () => {
     const storedWorkouts = localStorage.getItem('workoutData');
     if (storedWorkouts) {
-      const workouts = JSON.parse(storedWorkouts);
+      const workout = JSON.parse(storedWorkouts);
       workoutCardContainer.innerHTML = '';
-      workouts.forEach((entry) => {
-        createWorkoutElement(entry);
+
+      if (Array.isArray(workout)){
+        workout.forEach((entry) => {
+          createWorkoutElement(entry);
       });
-      overlay.classList.add('active');
-      if (!workoutHistoryCloseButton) {
-        workoutHistoryCloseButton = document.createElement('button');
-        workoutHistoryCloseButton.classList.add('close-button');
-        workoutHistoryCloseButton.innerHTML = '&times;';
-        workoutHistoryCloseButton.addEventListener('click', closeWorkoutHistory);
-        overlay.appendChild(workoutHistoryCloseButton);
+    } else{
+        createWorkoutElement(workout);
       }
+
     }
+    // Show the workoutCardContainer
+    workoutCardContainer.style.display = 'flex';
+      
+    overlay.classList.add('active');
+    if (!workoutHistoryCloseButton) {
+      workoutHistoryCloseButton = document.createElement('button');
+      workoutHistoryCloseButton.classList.add('close-button');
+      workoutHistoryCloseButton.innerHTML = '&times;';
+      workoutHistoryCloseButton.addEventListener('click', closeWorkoutHistory);
+      overlay.appendChild(workoutHistoryCloseButton);
+    }
+
   };
 
   const closeForm = () => {
@@ -144,36 +164,62 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   
-
+  
   // add workout button functionality
   addWorkoutButton.addEventListener('click', () => {
     openForm();
   });
-
-  // form submission
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const workoutDate = document.getElementById('workoutDate').value;
-    const startTime = document.getElementById('startTime').value;
-    const endTime = document.getElementById('endTime').value;
-    const easy = document.getElementById('easy').checked;
-    const moderate = document.getElementById('moderate').checked;
-    const difficult = document.getElementById('difficult').checked;
-    const style = document.getElementById('style').value;
-    addWorkoutEntry(workoutDate, startTime, endTime, easy, moderate, difficult, style);
-    createWorkoutElement({ workoutDate, startTime, endTime, easy, moderate, difficult, style });
-    closeForm();
-    form.reset();
-  });
-
   // display workout history
   workoutHistoryButton.addEventListener('click', () => {
     openWorkoutHistory();
   });
-});
 
+  // form submission
+  formGroup.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+  
+    const workoutDate = formGroup.elements['user-workout-date'].value;
+    const startTime = formGroup.elements['user-workout-start-time'].value;
+    const endTime = formGroup.elements['user-workout-end-time'].value;
+    const easy = document.getElementById('easy-input').checked; // Update the IDs for the difficulty inputs
+    const moderate = document.getElementById('moderate-input').checked;
+    const difficult = document.getElementById('difficult-input').checked;
+    const style = formGroup.elements['user-jump-types'].value;
+    
+    console.log('Workout Date:', workoutDate);
+    console.log('Start Time:', startTime);
+    console.log('End Time:', endTime);
+    console.log('Easy:', easy);
+    console.log('Moderate:', moderate);
+    console.log('Difficult:', difficult);
+    console.log('Style:', style);
+  
+    addWorkoutEntry(workoutDate, startTime, endTime, easy, moderate, difficult, style);
+    
+    createWorkoutElement({ workoutDate, startTime, endTime, easy, moderate, difficult, style });
+    // Store form data in local storage
+    localStorage.setItem('workoutData', JSON.stringify(workout));
+   
+    closeForm();
+    formGroup.reset();
+    
+    
+  //   const openPopup = () => {
+  //     popup.classList.add('active');
+  //     overlay.classList.add('active');
+  //   }
+    
+  //   openPopup();
 
+  //   setTimeout(() => {
+  //     closePopup();
+  //   }, 3000);
+  // });
 
-
-
-
+  // function closePopup(){
+  //   popup.classList.remove('active');
+  //   overlay.classList.remove('active');
+  // }
+  });
+})
